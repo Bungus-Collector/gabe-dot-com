a0887b101b7e98f6dc4288ff20b5a58d42051f37
Gabe <gabriel.lagace@mail.concordia.ca>
2025-01-26 14:07:53 -0500

    Initial commit

---
+++ personalWebsiteApp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home_page(request):
+    context = {"url": ""}
+    return render(request, 'personalWebsiteApp/index.html', context)
+
+def about_me_page(request):
+    context = {"url": "about-me"}
+    return render(request, 'personalWebsiteApp/index.html', context)
+
+def about_website_page(request):
+    context = {"url": "about-website"}
+    return render(request, 'personalWebsiteApp/index.html', context)
+
+def projects_page(request):
+    context = {"url": "projects"}
+    return render(request, 'personalWebsiteApp/index.html', context)
+
+def cv_page(request):
+    context = {"url": "cv"}
+    return render(request, 'personalWebsiteApp/index.html', context)
